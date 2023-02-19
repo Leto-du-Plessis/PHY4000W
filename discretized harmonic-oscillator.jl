0cecdb479f3776ea3c060955c5752b2417ee9267
Leto-du-Plessis <116193208+Leto-du-Plessis@users.noreply.github.com>
2023-02-19 19:48:03 +0200

    expanded code

--- discretized harmonic-oscillator.jl
+++ discretized harmonic-oscillator.jl
@@ -4,7 +4,7 @@ import LinearAlgebra.Diagonal as Diagonal
 # Settings
 # --------------------------------------
 
-N = 1000    # Number of discrete points
+N = 10    # Number of discrete points
 m = 1       # Mass
 omega = 1 
 Lbound = -3 # Left x - bound
@@ -14,21 +14,30 @@ deltax = (Rbound - Lbound)/N # x - step
 # Create kinetic energy matrix
 # --------------------------------------
 
-diagonal = vec(2 * ones(1, N))
-subdiagonal = vec(-1 * ones(1, N-1))
+diagonal = vec(2 * ones(1, N+1))
+subdiagonal = vec(-1 * ones(1, N))
 
 T = 1/(2 * m * deltax ^ 2) * Tridiagonal(subdiagonal, diagonal, subdiagonal) # Kinetic energy matrix
 
 # Create potential enery matrix
 # --------------------------------------
 
-diagonal = ones(1, N)
+diagonal = ones(1, N+1)
 x = Lbound
-for i = 1:N 
+for i = 1:N+1 
     diagonal[i] = x ^ 2
     global x = x + deltax
 end
 
 U = ((m * omega ^ 2)/2) * Diagonal(vec(diagonal))
 
-println(U)
+# Create Hamiltonian matrix
+# --------------------------------------
+
+H = T + U # Hamiltonian matrix
+
+# Calculate eigenvalues and eigenfunctions
+
+eigenvalues = eigvals(H)
+
+println(eigenvalues)
